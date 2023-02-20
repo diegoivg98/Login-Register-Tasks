17bdee48041b4f4ba31189a3a5f3e443b628f583
Diego Vistoso <diegoivg98@gmail.com>
2023-02-20 14:41:57 -0300

    agregar autorizacion a TaskForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import LoginForm from './components/LoginForm';
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import LoginForm from "./components/LoginForm";
+import RegisterForm from "./components/RegisterForm";
+import TaskForm from "./components/TaskForm";
+import "./App.css"
+
+
+/* Checking if the user is logged in. If they are, it will return the child component. If not, it will
+redirect them to the login page. */
+const requireAuth = (child) => {
+  const storedEmail = localStorage.getItem('email');
+  const storedPassword = localStorage.getItem('password');
+  if (storedEmail && storedPassword) {
+    <Navigate to="/tasks"/>
+    return child;
+  }
+  return <Navigate to="/" replace />;
+};
 
 function App() {
+
   return (
-   <BrowserRouter>
-     <Routes>
-        <Route path="/" element={ LoginForm } />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/tasks" element={requireAuth(<TaskForm />)} />
       </Routes>
-   </BrowserRouter>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
